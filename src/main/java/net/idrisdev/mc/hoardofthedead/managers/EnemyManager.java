da32c0efec762a5108f4ce022c0a2ba1818781e9
Idris <idrisdev@gmail.com>
2022-12-14 21:50:36 +1100

    feat (enemies): add EnemyManager to manage enemies.

---
+++ src/main/java/net/idrisdev/mc/hoardofthedead/managers/EnemyManager.java
@@ -0,0 +1,9 @@
+package net.idrisdev.mc.hoardofthedead.managers;
+
+public class EnemyManager {
+    private final WaveManager waveManager;
+
+    public EnemyManager(WaveManager waveManager){
+        this.waveManager = waveManager;
+    }
+}
