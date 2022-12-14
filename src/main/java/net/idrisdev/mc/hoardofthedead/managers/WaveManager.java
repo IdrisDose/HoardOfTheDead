43d43a7867d1e5dfa1923e9cd4035c69544bfde4
Idris <idrisdev@gmail.com>
2022-12-14 21:50:11 +1100

    feat (waves): add WaveManager to manage waves.

---
+++ src/main/java/net/idrisdev/mc/hoardofthedead/managers/WaveManager.java
@@ -0,0 +1,22 @@
+package net.idrisdev.mc.hoardofthedead.managers;
+
+public class WaveManager {
+   private int WaveNumber = 1;
+   private final GameManager gameManager;
+   public WaveManager(GameManager gameManager){
+      this.gameManager = gameManager;
+   }
+
+   public void startWave(){
+
+   }
+
+   public void nextWave(){
+
+   }
+
+   public void endWave(){
+
+   }
+
+}
