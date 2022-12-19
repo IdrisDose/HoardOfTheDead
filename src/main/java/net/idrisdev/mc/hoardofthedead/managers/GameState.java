1d65475ce6f9f87c52e6e6f81e954cb6a9df1685
Idris <idrisdev@gmail.com>
2022-12-20 01:04:29 +1100

    feat (gamestates): implemented new gamestates for the gamemanager to handle).

--- src/main/java/net/idrisdev/mc/hoardofthedead/managers/GameState.java
+++ src/main/java/net/idrisdev/mc/hoardofthedead/managers/GameState.java
@@ -1,5 +1,5 @@
 package net.idrisdev.mc.hoardofthedead.managers;
 
 public enum GameState {
-    WAITING, STARTING, ACTIVE, WON, LOSS, RESTARTING
+    WAITING, STARTING, ACTIVE, WON, LOSS, NEXT_WAVE, START_NEXT_WAVE, RESTARTING
 }
