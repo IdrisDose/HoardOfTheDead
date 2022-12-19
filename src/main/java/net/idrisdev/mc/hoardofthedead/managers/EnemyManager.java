d3b7a809384b5a6cd4bef531062e60574cbe506a
Idris <idrisdev@gmail.com>
2022-12-20 01:01:55 +1100

    refactor (managers): removed enemy manager.
    Migrated the enemy manager into the wave manager for easier enemy management.

--- src/main/java/net/idrisdev/mc/hoardofthedead/managers/EnemyManager.java
+++
@@ -1,9 +0,0 @@
-package net.idrisdev.mc.hoardofthedead.managers;
-
-public class EnemyManager {
-    private final WaveManager waveManager;
-
-    public EnemyManager(WaveManager waveManager){
-        this.waveManager = waveManager;
-    }
-}
